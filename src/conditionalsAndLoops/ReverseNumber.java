a525e4bc7245678fc2a1b7d17b13185ec1e13bc4
Sahil Sharma <Sahil.Sharma@in.ey.com>
2025-06-26 22:03:16 +0530

    formatting

--- src/conditionalsAndLoops/ReverseNumber.java
+++ src/conditionalsAndLoops/ReverseNumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ReverseNumber {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number : ");
